fb52659f8bcb0f3bb75e8d4eb1f187337a0c4a6d
baudneo <baudneo@protonmail.com>
2021-12-03 16:05:57 -0700

    testing

--- rootfs/etc/cont-init.d/30-mlapi-config.sh
+++ rootfs/etc/cont-init.d/30-mlapi-config.sh
@@ -4,30 +4,7 @@
 program_name="mlapi-config"
 
 if [ ! -f /config/mlapi_dbuser.py ]; then
-    logger -s "INFO: ${program_name}: /config/mlapi_dbuser.py not found, creating..."
-      s6-setuidgid www-data cp /mlapi/mlapi_dbuser.py /config
-  s6-setuidgid www-data cp /mlapi/mlapi_face_train.py /config
-  s6-setuidgid www-data cp /mlapi/get_encryption_key.py /config
-  s6-setuidgid www-data cp /mlapi/get_models.sh /config
-  s6-setuidgid www-data cp /mlapi/mlapiconfig.yml /config
-  s6-setuidgid www-data cp /mlapi/mlapisecrets.yml /config
-  s6-setuidgid www-data cp -r /mlapi/images/ /config
-  s6-setuidgid www-data cp -r /mlapi/known_faces/ /config
-  s6-setuidgid www-data cp -r /mlapi/unknown_faces/ /config
-  s6-setuidgid www-data cp -r /mlapi/tools/ /config
-  s6-setuidgid www-data cp -r /mlapi/models /config
-  s6-setuidgid www-data cp -r /mlapi/logs/ /config
-  s6-setuidgid www-data cp -r /mlapi/db/ /config
-  s6-setuidgid www-data cp -r /mlapi/examples/ /config
-  s6-setuidgid www-data cp -r /mlapi/tools/ /config
-fi
-
-# Configure mlapi config folder
-if [ ! -d "/config" ]; then
-  echo "Configuring Neo MLAPI Configuration folder" | init "[${program_name}] "
-
-  s6-setuidgid www-data mkdir -p /config
-#  s6-setuidgid www-data mkdir -p /config/models
+  echo "/config/mlapi_dbuser.py not found, creating base config..." | init "[${program_name}]"
   s6-setuidgid www-data cp /mlapi/mlapi_dbuser.py /config
   s6-setuidgid www-data cp /mlapi/mlapi_face_train.py /config
   s6-setuidgid www-data cp /mlapi/get_encryption_key.py /config
@@ -43,7 +20,6 @@ if [ ! -d "/config" ]; then
   s6-setuidgid www-data cp -r /mlapi/db/ /config
   s6-setuidgid www-data cp -r /mlapi/examples/ /config
   s6-setuidgid www-data cp -r /mlapi/tools/ /config
-  chmod -R 755 /config
   chown -R www-data:www-data /config
 fi
 
@@ -54,20 +30,23 @@ sed -i "s|#wsgi_server: bjoern|wsgi_server: bjoern|" /config/mlapiconfig.yml
 sed -i "s|#log_user:.*|log_user: www-data|" /config/mlapiconfig.yml
 sed -i "s|#log_group:.*|log_group: www-data|" /config/mlapiconfig.yml
 sed -i "s|#log_path:.*|log_path: /log|" /config/mlapiconfig.yml
-# db user
+# db user, ensures there is always at least 1 default user
 python3 /config/mlapi_dbuser.py --force -c /config/mlapiconfig.yml -d /config/db -u ${MLAPIDB_USER} -p ${MLAPIDB_PASS}
-# MLAPI_SECRET_KEY: MAKE me something unique I am for signing JWT
-if [ "${USE_SECURE_RANDOM_ORG}" -eq 1 ]; then
-    echo "Fetching random secure string for MLAPI JWT signing key from random.org..." | init
-    random_string=$(
-      wget -qO - \
-        "https://www.random.org/strings/?num=4&len=20&digits=on&upperalpha=on&loweralpha=on&unique=on&format=plain&rnd=new" \
-      | tr -d '\n' \
-    )
-  else
-    echo "Generating standard random string for MLAPI JWT signing key..." | init
-    random_string="$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM"
+# If the JWT key is default, create a new one (dont need to create one every restart)
+jwt_needed=$(grep "MLAPI_SECRET_KEY: MAKE me something" < /config/mlapisecrets.yml)
+if [ -z "${jwt_needed}" ]; then
+  if [ "${USE_SECURE_RANDOM_ORG}" -eq 1 ]; then
+      echo "Fetching random secure string for MLAPI JWT signing key from random.org..." | init
+      random_string=$(
+        wget -qO - \
+          "https://www.random.org/strings/?num=4&len=20&digits=on&upperalpha=on&loweralpha=on&unique=on&format=plain&rnd=new" \
+        | tr -d '\n' \
+      )
+    else
+      echo "Generating standard random string for MLAPI JWT signing key..." | init
+      random_string="$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM"
+  fi
+  sed -i "s|MLAPI_SECRET_KEY:.*|MLAPI_SECRET_KEY: \"${random_string}\"|" /config/mlapisecrets.yml
 fi
-sed -i "s|MLAPI_SECRET_KEY:.*|MLAPI_SECRET_KEY: \"${random_string}\"|" /config/mlapisecrets.yml
 
 
